43ff8a1991088c78601149cf08fadec50b865eb9
Milan <milan@blockchain.com>
2021-05-29 00:01:58 +0200

    feat(qr): fixed formatting in files

--- packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/LinkedBanks/selectors.ts
+++ packages/blockchain-wallet-v4-frontend/src/scenes/Settings/General/LinkedBanks/selectors.ts
@@ -1,25 +1,18 @@
 import { lift } from 'ramda'
 
-import { ExtractSuccess } from 'blockchain-wallet-v4/src/types'
-import { InvitationsType } from 'core/types'
+import { ExtractSuccess, InvitationsType } from 'blockchain-wallet-v4/src/types'
 import { selectors } from 'data'
 import { RootState } from 'data/rootReducer'
 
-export const getData = (state: RootState) => {
-  const bankAccountsR = selectors.components.brokerage.getBankTransferAccounts(
-    state
-  )
+const getData = (state: RootState) => {
+  const bankAccountsR = selectors.components.brokerage.getBankTransferAccounts(state)
 
-  const paymentMethodsR = selectors.components.simpleBuy.getSBPaymentMethods(
-    state
-  )
+  const paymentMethodsR = selectors.components.simpleBuy.getSBPaymentMethods(state)
 
   // TODO: Remove this when Open Banking gets rolled out 100%
-  const invitations: InvitationsType = selectors.core.settings
-    .getInvitations(state)
-    .getOrElse({
-      openBanking: false
-    } as InvitationsType)
+  const invitations: InvitationsType = selectors.core.settings.getInvitations(state).getOrElse({
+    openBanking: false
+  } as InvitationsType)
   return lift(
     (
       bankAccounts: ExtractSuccess<typeof bankAccountsR>,
@@ -29,7 +22,7 @@ export const getData = (state: RootState) => {
       paymentMethods:
         (!invitations.openBanking && {
           ...paymentMethods,
-          methods: paymentMethods.methods.filter(m => {
+          methods: paymentMethods.methods.filter((m) => {
             return m.type === 'BANK_ACCOUNT' || m.currency === 'USD'
           })
         }) ||
@@ -37,3 +30,5 @@ export const getData = (state: RootState) => {
     })
   )(bankAccountsR, paymentMethodsR)
 }
+
+export default getData
